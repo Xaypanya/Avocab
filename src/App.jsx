250fc77194dccd739d3eec3d4f20f80bc32e22ae
Xaypanya Phongsa <dackogan@gmail.com>
2024-09-11 01:15:32 +0700

    update word card width

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     },
     {
       "id": 2,
-      "lao": "ເຕົາອບ",
+      "lao": "ເຕົາອົບ",
       "english": "cooker",
       "type": "noun",
       "favorite": false
@@ -36,7 +36,7 @@ function App() {
     {
       "id": 3,
       "lao": "ສຳເນົາ",
-      "english": "copy",
+      "english": "Copy",
       "type": "noun, verb",
       "favorite": false
     },
@@ -61,118 +61,7 @@ function App() {
       "type": "verb",
       "favorite": false
     },
-    {
-      "id": 7,
-      "lao": "ຄູ່",
-      "english": "couple",
-      "type": "noun",
-      "favorite": false
-    },
-    {
-      "id": 8,
-      "lao": "ປົກຄຸມ",
-      "english": "cover",
-      "type": "verb",
-      "favorite": false
-    },
-    {
-      "id": 9,
-      "lao": "ບ້າ",
-      "english": "crazy",
-      "type": "adjective",
-      "favorite": false
-    },
-    {
-      "id": 10,
-      "lao": "ສ້າງສັນ",
-      "english": "creative",
-      "type": "adjective",
-      "favorite": false
-    },
-    {
-      "id": 11,
-      "lao": "ເຄຣິດິດ",
-      "english": "credit",
-      "type": "noun",
-      "favorite": false
-    },
-    {
-      "id": 12,
-      "lao": "ອາຊະຍາກຳ",
-      "english": "crime",
-      "type": "noun",
-      "favorite": false
-    },
-    {
-      "id": 13,
-      "lao": "ອາຊະຍາກອນ",
-      "english": "criminal",
-      "type": "noun, adjective",
-      "favorite": false
-    },
-    {
-      "id": 14,
-      "lao": "ຂໍ້ມູນ",
-      "english": "cross",
-      "type": "verb, noun",
-      "favorite": false
-    },
-    {
-      "id": 15,
-      "lao": "ຝູງຊົນ",
-      "english": "crowd",
-      "type": "noun",
-      "favorite": false
-    },
-    {
-      "id": 16,
-      "lao": "ແອອດ",
-      "english": "crowded",
-      "type": "adjective",
-      "favorite": false
-    },
-    {
-      "id": 17,
-      "lao": "ຮ້ອງໄຫ້",
-      "english": "cry",
-      "type": "verb",
-      "favorite": false
-    },
-    {
-      "id": 18,
-      "lao": "ຕູ້ເກັບສິນຄ້າ",
-      "english": "cupboard",
-      "type": "noun",
-      "favorite": false
-    },
-    {
-      "id": 19,
-      "lao": "ຫຍິກ",
-      "english": "curly",
-      "type": "adjective",
-      "favorite": false
-    },
-    {
-      "id": 20,
-      "lao": "ວົນຈອນ",
-      "english": "cycle",
-      "type": "noun, verb",
-      "favorite": false
-    },
-    {
-      "id": 21,
-      "lao": "ປະຈໍາມື້",
-      "english": "daily",
-      "type": "adjective",
-      "favorite": false
-    },
-    {
-      "id": 22,
-      "lao": "ອັນຕະລາຍ",
-      "english": "danger",
-      "type": "noun",
-      "favorite": false
-    }
+ 
   ]);
 
   const toggleFavorite = (id) => {
@@ -200,7 +89,7 @@ function App() {
         backgroundSize: "20px 20px",
       }}
     >
-      <div className="w-5/6 lg:w-3/4 xl:w-2/4 2xl:1/4 mx-auto">
+      <div className="w-5/6 md:3/6 lg:w-2/4 xl:w-1/4 2xl:1/4 mx-auto">
         <HeaderBar isEnglish={isEnglish} setSearchTerm={setSearchTerm} />
 
         <div className="fixed top-14 right-6 space-x-2">
--- src/components/WordCard.jsx
+++ src/components/WordCard.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from "react";
 import { Heart, Volume2 } from "lucide-react";
-import { LAOS_FLAG, UK_FLAG } from '../constants';
+import { LAOS_FLAG, UK_FLAG } from "../constants";
 
 const WordCard = ({ word, toggleFavorite }) => {
-
   const playAudio = () => {
     const synth = window.speechSynthesis;
-  
+
     const getVoices = () => {
       const voices = synth.getVoices();
       const utterance = new SpeechSynthesisUtterance(word.english);
-      utterance.lang = 'en-US';
+      utterance.lang = "en-US";
       utterance.voice = voices[5]; // Adjust index based on available voices
       utterance.volume = 1; // Volume range = 0 - 1
       utterance.rate = 1; // Speed of the text read , default 1
       synth.speak(utterance);
     };
-  
+
     if (synth.getVoices().length === 0) {
       // Voices not loaded yet
-      synth.addEventListener('voiceschanged', getVoices);
+      synth.addEventListener("voiceschanged", getVoices);
     } else {
       // Voices already loaded
       getVoices();
@@ -30,30 +29,48 @@ const WordCard = ({ word, toggleFavorite }) => {
       <div className="flex justify-between items-start">
         <div className="flex-grow">
           <div className="flex items-center space-x-2 mb-3">
-            <img src={UK_FLAG} alt='UK Flag' className="w-6 h-6 bg-red-500 rounded-full border border-black"/>
+            <img
+              src={UK_FLAG}
+              alt="UK Flag"
+              className="w-6 h-6 bg-red-500 rounded-full border border-black"
+            />
             <h2 className="text-lg font-bold">{word.english}</h2>
-            <button 
-              variant="outline" 
-              size="icon" 
-              className="p-1 border-2 border-black rounded-full bg-yellow-300 hover:bg-yellow-400 transition-colors" 
-              onClick={playAudio}
-            >
-              <Volume2 className="h-4 w-4" />
-            </button>
           </div>
           <div className="flex items-center space-x-2 mb-3">
-            <img src={LAOS_FLAG} alt='UK Flag' className="w-6 h-6 bg-blue-500 rounded-full border border-black"/>
+            <img
+              src={LAOS_FLAG}
+              alt="UK Flag"
+              className="w-6 h-6 bg-blue-500 rounded-full border border-black"
+            />
             <p className="text-md">{word.lao}</p>
           </div>
-          <p className="text-sm text-gray-600 mt-2 bg-gray-100 inline-block px-2 py-1 rounded-full border border-gray-300">{word.type}</p>
+          <p className="text-sm text-gray-600 mt-2 bg-gray-100 inline-block px-2 py-1 rounded-full border border-gray-300">
+            {word.type}
+          </p>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <button
+            variant="outline"
+            size="icon"
+            className="p-1 border-2 border-black rounded-lg bg-yellow-300 hover:bg-yellow-400 active:bg-yellow-500 transition-colors"
+            onClick={playAudio}
+          >
+            <Volume2 className="h-7 w-7" />
+          </button>
           <button
             onClick={toggleFavorite}
             variant="outline"
-            className={`p-1 ${word.favorite ? 'bg-red-500' : 'bg-white'} border-2 border-black rounded-full w-10 h-10 flex items-center justify-center`}
+            className={`p-1 ${
+              word.favorite ? "bg-red-500" : "bg-white"
+            } border-2 border-black rounded-full w-10 h-10 flex items-center justify-center`}
           >
-            <Heart className={`h-5 w-5 ${word.favorite ? 'fill-white stroke-white' : 'fill-none stroke-black'}`} />
+            <Heart
+              className={`h-5 w-5 ${
+                word.favorite
+                  ? "fill-white stroke-white"
+                  : "fill-none stroke-black"
+              }`}
+            />
           </button>
         </div>
       </div>
